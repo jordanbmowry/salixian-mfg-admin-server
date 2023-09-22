0044d60d0ffbc978e281acd2b4d19a5e41c855f5
Joran Mowry <jordan.mowry@gmail.com>
2023-09-22 19:20:25 -0400

    refactor

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,15 @@ import usersRouter from './resources/users/users.router';
 import cookieParser from 'cookie-parser';
 import pinoHttp from 'pino-http';
 import logger from './config/logger';
-
-const httpLogger = pinoHttp({ logger });
-
 import type { CustomError } from './types/types';
 
 const app: Application = express();
 
 app.use(express.json());
-
-app.use(httpLogger);
-
+app.use(pinoHttp({ logger }));
 app.use(cookieParser());
 
+// routes
 app.use('/users', usersRouter);
 
 // Not found handler
