b5b3001092c225139944dc0456591877f4d023db
Joran Mowry <jordan.mowry@gmail.com>
2023-09-22 17:32:41 -0400

    get token from cookie

--- src/auth/authMiddleware.ts
+++ src/auth/authMiddleware.ts
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt, { VerifyErrors } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
 export const authenticateJWT = (
@@ -6,7 +6,7 @@ export const authenticateJWT = (
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.header('Authorization')?.split(' ')[1];
+  const token = req.cookies?.token;
 
   if (!token) {
     return res
@@ -14,22 +14,26 @@ export const authenticateJWT = (
       .json({ message: 'Access denied. No token provided.' });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY!, (err, user) => {
-    if (err) {
-      if (err.name === 'TokenExpiredError') {
-        return res.status(403).json({ message: 'Token has expired' });
+  jwt.verify(
+    token,
+    process.env.JWT_SECRET_KEY!,
+    (err: VerifyErrors | null, decoded: any) => {
+      if (err) {
+        if (err.name === 'TokenExpiredError') {
+          return res.status(403).json({ message: 'Token has expired' });
+        }
+        if (err.name === 'JsonWebTokenError') {
+          return res.status(401).json({ message: 'Unauthorized' });
+        }
+        return res.sendStatus(403);
       }
-      if (err.name === 'JsonWebTokenError') {
-        return res.status(401).json({ message: 'Unauthorized' });
-      }
-      return res.sendStatus(403);
-    }
 
-    if (typeof user === 'object' && user !== null) {
-      req.user = user as Record<string, any>;
-      next();
-    } else {
-      res.status(400).json({ message: 'Invalid token.' });
+      if (decoded) {
+        (req as any).user = decoded as Record<string, any>;
+        next();
+      } else {
+        res.status(400).json({ message: 'Invalid token.' });
+      }
     }
-  });
+  );
 };
