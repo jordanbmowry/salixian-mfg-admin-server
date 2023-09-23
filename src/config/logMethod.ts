e93951c9ad5766d785be61d787b916634f104777
Joran Mowry <jordan.mowry@gmail.com>
2023-09-23 19:07:29 -0400

    add info to logMethod

--- src/config/logMethod.ts
+++ src/config/logMethod.ts
@@ -1,7 +1,19 @@
 import { Request } from 'express';
 
 export function logMethod(req: Request, methodName: string, data?: any) {
-  req.log.debug({ __filename, methodName });
+  const { originalUrl, method, headers, body } = req;
+
+  const headersToLog =
+    process.env.NODE_ENV === 'development' ? headers : undefined;
+
+  req.log.debug({
+    __filename,
+    methodName,
+    originalUrl,
+    method,
+    headers: headersToLog,
+  });
+
   if (data) {
     req.log.trace({ __filename, methodName, return: true, data });
   }
