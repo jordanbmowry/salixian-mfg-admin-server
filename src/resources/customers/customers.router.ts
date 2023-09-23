e0011d74489c2988d6be1393a2e3e3a642cd1c41
Joran Mowry <jordan.mowry@gmail.com>
2023-09-23 15:10:09 -0400

    customers router

--- src/resources/customers/customers.router.ts
+++ src/resources/customers/customers.router.ts
@@ -3,9 +3,23 @@ import controller from './customers.controller';
 import methodNotAllowed from '../../errors/methodNotAllowed';
 
 const CUSTOMERS_BASE_ROUTE = '/';
+const CUSTOMER_ID_ROUTE = '/:customerId([a-fA-F0-9-]{36}|[0-9]+)';
+const SOFT_DELETE_ROUTE = '/soft-delete/:customerId([a-fA-F0-9-]{36}|[0-9]+)';
+const HARD_DELETE_ROUTE = '/soft-delete/:customerId([a-fA-F0-9-]{36}|[0-9]+)';
 
 const router = Router();
 
 router.route(CUSTOMERS_BASE_ROUTE).get(controller.list).all(methodNotAllowed);
+
+router
+  .route(CUSTOMER_ID_ROUTE)
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
+
+router
+  .route(SOFT_DELETE_ROUTE)
+  .put(controller.softDelete)
+  .all(methodNotAllowed);
 
 export default router;
