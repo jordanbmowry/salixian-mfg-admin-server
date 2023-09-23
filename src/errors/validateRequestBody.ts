1fc006e6e04388f75310b1cbe4afc88f47447a60
Joran Mowry <jordan.mowry@gmail.com>
2023-09-22 23:03:17 -0400

    add error middleware for future use

---
+++ src/errors/validateRequestBody.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+
+export function validateRequestBody(schema: Joi.Schema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+    next();
+  };
+}
