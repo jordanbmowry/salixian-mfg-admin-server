51859a857970874ed4bd3f68d6d0c064a3d190f8
Joran Mowry <jordan.mowry@gmail.com>
2023-09-25 11:50:50 -0400

    check for array and handle sanitize

--- src/utils/sanitizeMiddleware.ts
+++ src/utils/sanitizeMiddleware.ts
@@ -1,15 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import xss from 'xss';
 
-// Recursive function to sanitize all string properties in an object
+// Recursive function to sanitize all string properties in an object or array
 function sanitize(obj: any): any {
-  if (typeof obj === 'object' && obj !== null) {
-    // If obj is an object, sanitize all of its properties
+  // If obj is an array, sanitize each element in the array
+  if (Array.isArray(obj)) {
+    return obj.map((el) => sanitize(el));
+  }
+  // If obj is an object, sanitize all of its properties
+  else if (typeof obj === 'object' && obj !== null) {
     for (const key in obj) {
       obj[key] = sanitize(obj[key]);
     }
-  } else if (typeof obj === 'string') {
-    // If obj is a string, sanitize the string
+  }
+  // If obj is a string, sanitize the string
+  else if (typeof obj === 'string') {
     return xss(obj);
   }
   return obj;
